99861fe45d34527016cf8a465695b9bfee0efefe
Aminata11 <123710156+Aminata11@users.noreply.github.com>
2025-10-22 00:32:10 +0000

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,36 @@
+variable "region" {
+  description = "AWS region à utiliser"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "instance_type" {
+  description = "Type d’instance EC2"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "aws_access_key" {
+  description = "Access key AWS"
+  type        = string
+  default     = ""
+}
+
+variable "aws_secret_key" {
+  description = "Secret key AWS"
+  type        = string
+  default     = ""
+}
+
+variable "aws_session_token" {
+  description = "Session token AWS (si fourni)"
+  type        = string
+  default     = ""
+}
+
+variable "ec2_key_name" {
+  description = "Nom de la clé SSH existante"
+  type        = string
+  default     = "lab-key"
+}
 
