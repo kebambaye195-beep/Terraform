a04c4452a459d672e9be091d424d9990b0e93d55
Aminata11 <123710156+Aminata11@users.noreply.github.com>
2025-10-23 21:41:02 +0000

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,10 +1,9 @@
 output "ec2_public_ip" {
   description = "Adresse IP publique de l’instance EC2"
-  value       = aws_instance.lab_ec2.public_ip
+  value       = aws_instance.web.public_ip
 }
 
-output "dynamodb_table_name" {
-  description = "Nom de la table DynamoDB créée"
-  value       = aws_dynamodb_table.lab_table.name
+output "rds_endpoint" {
+  description = "Endpoint de la base de données RDS"
+  value       = aws_db_instance.db.endpoint
 }
-
